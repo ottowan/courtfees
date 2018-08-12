99fdc247dfc2be8b2044234dd4f2ac118c784b1b
parinya <parinya.coj@gmail.com>
2018-08-12 21:41:35 +0700

    [Edited]: ลบค่า failFeePrice เพื่อนำไปเป็นค่า global แทน

--- src/generalfees/calculatecapitalequal50milion.go
+++ src/generalfees/calculatecapitalequal50milion.go
@@ -3,12 +3,11 @@ package generalfees
 func CalculateCapitalEqual50Milion(feeType string, feeCapital float64) float64 {
 
 	const feePrice = 200000.00
-	const failFeePrice = 0.00
 
 	if feeType == feeTypeConst && feeCapital == feeCapitalConst {
 		return feePrice
 	}
 
-	return failFeePrice
+	return failFeePriceConst
 
 }
--- src/generalfees/calculatecapitalover50milion.go
+++ src/generalfees/calculatecapitalover50milion.go
@@ -2,16 +2,16 @@ package generalfees
 
 const feeCapitalConst = 50000000.00
 const feeTypeConst = "มีทุนทรัพย์"
+const failFeePriceConst = 0.00
 
 func CalculateCapitalOver50Milion(feeType string, feeCapital float64) float64 {
 
 	const feePrice = 200000.00
-	const failFeePrice = 0.00
 
 	if feeType == feeTypeConst && feeCapital > feeCapitalConst {
 		return feePrice + (((feeCapital - feeCapitalConst) * 0.1) / 100)
 	}
 
-	return failFeePrice
+	return failFeePriceConst
 
 }
