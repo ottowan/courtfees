34223d87fda5485e160847ab012581796fad4096
parinya <parinya.coj@gmail.com>
2018-08-12 16:29:17 +0700

    [Edited]: เปลี่ยนตัวแปรค่าคงที่ เอาไปไว้เป็นตัว global variable

--- src/generalfees/calculatecapitalequal50milion.go
+++ src/generalfees/calculatecapitalequal50milion.go
@@ -2,8 +2,6 @@ package generalfees
 
 func CalculateCapitalEqual50Milion(feeType string, feeCapital float64) float64 {
 
-	const feeTypeConst = "มีทุนทรัพย์"
-	const feeCapitalConst = 50000000.00
 	const feePrice = 200000.00
 	const failFeePrice = 0.00
 
