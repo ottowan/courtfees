050b0eb0940ba722d4985eca480a0f616408196e
parinya <parinya.coj@gmail.com>
2018-08-12 21:49:06 +0700

    [Created]: สร้างไฟล์สำหรับตรวจสอบค่า ธรรมเนียมว่ามีค่าเกิน 200k หรือไม่

---
+++ src/generalfees/checkfeepriceover200k.go
@@ -0,0 +1,11 @@
+package generalfees
+
+func CheckFeePriceOver200k(feePrice float64) float64 {
+
+	const feePriceConst = 200000.00
+	if feePrice > feePriceConst {
+		return feePriceConst
+	} else {
+		return feePrice
+	}
+}
---
+++ src/generalfees/checkfeepriceover200k_test.go
@@ -0,0 +1,14 @@
+package generalfees
+
+import "testing"
+
+func Test_CheckFeePriceOver200k_Input_913600_Should_Be_200k(t *testing.T) {
+	expectedFeePrice := 200000.00
+	feePrice := 913600.00
+
+	actualFeePrice := CheckFeePriceOver200k(feePrice)
+
+	if expectedFeePrice != actualFeePrice {
+		t.Errorf("Expected %v but got it %v", expectedFeePrice, actualFeePrice)
+	}
+}
