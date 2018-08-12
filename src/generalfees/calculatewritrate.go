b071ec05e7d9e14710e62f92a7e6c81ec52ccad9
parinya <parinya.coj@gmail.com>
2018-08-13 00:53:37 +0700

    [Created]: สร้างไฟล์สำหรับเรียกใช้ API ของระบบอัตราค่านำหมาย

---
+++ src/generalfees/calculatewritrate.go
@@ -0,0 +1,43 @@
+package generalfees
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+
+type Data struct {
+    WritRate float64 `json:"writrate"`
+}
+
+func CalculateWritRate(moo int, subDistrict string, district string, province string) float64 {
+
+	url := "http://localhost:3000/writrate"
+	values := map[string]string{"moo": strconv.Itoa(moo), "subDistrict": subDistrict, "district": district, "province": province}
+	jsonValue, _ := json.Marshal(values)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var datas Data
+	json.Unmarshal(body, &datas)
+
+	// i, err := strconv.ParseFloat(datas[0].WritRate, 64)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	return datas.WritRate
+}
---
+++ src/generalfees/calculatewritrate_test.go
@@ -0,0 +1,17 @@
+package generalfees
+
+import "testing"
+
+func Test_CalculateWritRate_Input_5_maelaos_chiangkham_phayao_Should_Be_500(t *testing.T) {
+	expectedWritRate := 500.00
+	moo := 2
+	subDistrict := "แม่ลาว"
+	district := "เชียงคำ"
+	province := "พะเยา"
+
+	actualWritRate := CalculateWritRate(moo, subDistrict, district, province)
+
+	if expectedWritRate != actualWritRate {
+		t.Errorf("Expected %v but got it %v", expectedWritRate, actualWritRate)
+	}
+}
