17175f82e2aaccb3834a51147dfc2f76053a456c
parinya <parinya.coj@gmail.com>
2018-08-13 22:07:35 +0700

    [Edited]: แก้ไข merge file

--- src/havecapital/havcapital.go
+++ src/havecapital/havcapital.go
@@ -1,5 +1,18 @@
 package havecapital
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
 
+type Data struct {
+	WritRate float64 `json:"writrate"`
+}
+
+const url = "http://localhost:3000/writrate"
 const feeCapitalConst = 50000000.00
 const feeTypeConst = "มีทุนทรัพย์"
 const failFeePriceConst = 0.00
@@ -39,4 +52,31 @@ func CalculateCapitalOver50Milion(feeType string, feeCapital float64) float64 {
 
 	return failFeePriceConst
 
+}
+
+func CalculateWritRate(moo int, subDistrict string, district string, province string) float64 {
+
+	values := map[string]string{"moo": strconv.Itoa(moo), "subDistrict": subDistrict, "district": district, "province": province}
+	jsonValue, _ := json.Marshal(values)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var datas Data
+	json.Unmarshal(body, &datas)
+
+	// i, err := strconv.ParseFloat(datas[0].WritRate, 64)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+
+	return datas.WritRate
 }
--- src/havecapital/havecapital_test.go
+++ src/havecapital/havecapital_test.go
@@ -40,3 +40,17 @@ func Test_CalculateCapitalOver50Milion_Input_HaveCapital_650Milion200k_Shoud_Be_
 	}
 
 }
+
+func Test_CalculateWritRate_Input_5_maelaos_chiangkham_phayao_Should_Be_500(t *testing.T) {
+	expectedWritRate := 500.00
+	moo := 2
+	subDistrict := "แม่ลาว"
+	district := "เชียงคำ"
+	province := "พะเยา"
+
+	actualWritRate := CalculateWritRate(moo, subDistrict, district, province)
+
+	if expectedWritRate != actualWritRate {
+		t.Errorf("Expected %v but got it %v", expectedWritRate, actualWritRate)
+	}
+}
