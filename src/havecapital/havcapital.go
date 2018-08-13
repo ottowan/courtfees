86eae690191f7b8dcf00382679f6c1bb4d1c3875
parinya <parinya.coj@gmail.com>
2018-08-13 22:02:54 +0700

    [Edited]: แก้ไข merge file

--- src/havecapital/havcapital.go
+++ src/havecapital/havcapital.go
@@ -1,5 +1,9 @@
 package havecapital
 
+const feeCapitalConst = 50000000.00
+const feeTypeConst = "มีทุนทรัพย์"
+const failFeePriceConst = 0.00
+
 func CalculateCapitalEqual50Milion(feeType string, feeCapital float64) float64 {
 
 	const feePrice = 200000.00
@@ -12,13 +16,25 @@ func CalculateCapitalEqual50Milion(feeType string, feeCapital float64) float64 {
 
 }
 
-
 func CalculateCapitalLess50Milion(feeType string, feeCapital float64) float64 {
 
 	const feePrice = 200000.00
 
 	if feeType == feeTypeConst && feeCapital < feeCapitalConst {
 		return (feeCapital * 2) / 100
+	}
+
+	return failFeePriceConst
+
+}
+
+
+func CalculateCapitalOver50Milion(feeType string, feeCapital float64) float64 {
+
+	const feePrice = 200000.00
+
+	if feeType == feeTypeConst && feeCapital > feeCapitalConst {
+		return feePrice + (((feeCapital - feeCapitalConst) * 0.1) / 100)
 	}
 
 	return failFeePriceConst
--- src/havecapital/havecapital_test.go
+++ src/havecapital/havecapital_test.go
@@ -15,14 +15,25 @@ func Test_CalculateCapitalEqual50Milion_Input_HaveCapital_50Milion_Shoud_Be_200k
 
 }
 
-
-
 func Test_CalculateCapitalLess50Milion_Input_HaveCapital_45680000_Shoud_Be_913600(t *testing.T) {
 	expectedFeePrice := 913600.00
 	feeType := "มีทุนทรัพย์"
 	feeCapital := 45680000.00
 
 	actualFeePrice := CalculateCapitalLess50Milion(feeType, feeCapital)
+
+	if expectedFeePrice != actualFeePrice {
+		t.Errorf("Expected %v but got it %v", expectedFeePrice, actualFeePrice)
+	}
+
+}
+
+func Test_CalculateCapitalOver50Milion_Input_HaveCapital_650Milion200k_Shoud_Be_800200(t *testing.T) {
+	expectedFeePrice := 800200.00
+	feeType := "มีทุนทรัพย์"
+	feeCapital := 650200000.00
+
+	actualFeePrice := CalculateCapitalOver50Milion(feeType, feeCapital)
 
 	if expectedFeePrice != actualFeePrice {
 		t.Errorf("Expected %v but got it %v", expectedFeePrice, actualFeePrice)
