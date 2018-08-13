c76effc62e5a63d32746510aab3ee39509a7ef8e
parinya <parinya.coj@gmail.com>
2018-08-13 21:58:02 +0700

    [Edited]: แก้ไข merge file

--- src/havecapital/havcapital.go
+++ src/havecapital/havcapital.go
@@ -11,3 +11,16 @@ func CalculateCapitalEqual50Milion(feeType string, feeCapital float64) float64 {
 	return failFeePriceConst
 
 }
+
+
+func CalculateCapitalLess50Milion(feeType string, feeCapital float64) float64 {
+
+	const feePrice = 200000.00
+
+	if feeType == feeTypeConst && feeCapital < feeCapitalConst {
+		return (feeCapital * 2) / 100
+	}
+
+	return failFeePriceConst
+
+}
--- src/havecapital/havecapital_test.go
+++ src/havecapital/havecapital_test.go
@@ -14,3 +14,18 @@ func Test_CalculateCapitalEqual50Milion_Input_HaveCapital_50Milion_Shoud_Be_200k
 	}
 
 }
+
+
+
+func Test_CalculateCapitalLess50Milion_Input_HaveCapital_45680000_Shoud_Be_913600(t *testing.T) {
+	expectedFeePrice := 913600.00
+	feeType := "มีทุนทรัพย์"
+	feeCapital := 45680000.00
+
+	actualFeePrice := CalculateCapitalLess50Milion(feeType, feeCapital)
+
+	if expectedFeePrice != actualFeePrice {
+		t.Errorf("Expected %v but got it %v", expectedFeePrice, actualFeePrice)
+	}
+
+}
