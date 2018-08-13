1b5c60f125a48e67cc64f50ad9d55d9ec4656cfa
parinya <parinya.coj@gmail.com>
2018-08-13 07:57:41 +0700

    [Edited]: แก้ไขการ return ค่า

--- src/havecapital/checkfeepriceover200k.go
+++ src/havecapital/checkfeepriceover200k.go
@@ -5,7 +5,8 @@ func CheckFeePriceOver200k(feePrice float64) float64 {
 	const feePriceConst = 200000.00
 	if feePrice > feePriceConst {
 		return feePriceConst
-	} else {
-		return feePrice
 	}
+
+	return feePrice
+
 }
