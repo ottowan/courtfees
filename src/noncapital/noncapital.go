cb70ab83ebd7bf6d7ac40d5beff977f123a4c869
parinya <parinya.coj@gmail.com>
2018-08-21 15:47:11 +0700

    [Edited]: สร้างไฟล์รวม

--- src/noncapital/noncapital.go
+++ src/noncapital/noncapital.go
@@ -15,3 +15,15 @@ func InitFeePrice0() int {
 
 	return 0
 }
+
+func CheckSectionType288_1(section string) bool {
+
+	sectionReturn := "288(1)"
+	if section == sectionReturn {
+		return true
+	}
+
+	return false
+
+}
+
--- src/noncapital/noncapital_test.go
+++ src/noncapital/noncapital_test.go
@@ -24,3 +24,15 @@ func Test_InitFeePrice0_Input_Should_Be_0(t *testing.T) {
 
 	}
 }
+
+func Test_CheckSectionType288_1_Input_section_288_1_Should_Be_TRUE(t *testing.T) {
+
+	section := "288(1)"
+	expectedSection := true
+	actualSection := CheckSectionType288_1(section)
+
+	if expectedSection != actualSection {
+		t.Errorf("Expected %v but got it %v", expectedSection, actualSection)
+	}
+
+}
