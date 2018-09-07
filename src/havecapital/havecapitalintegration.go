d2147afba300ffe240f4b943449f2fd3344e66d6
parinya <parinya.coj@gmail.com>
2018-09-07 17:51:36 +0700

    [Create] index.html for jenkins report

---
+++ src/havecapital/havecapitalintegration.go
@@ -0,0 +1,5 @@
+package havecapital
+
+func CalculateHaveCapital() {
+
+}
---
+++ src/havecapital/havecapitalintegration_test.go
@@ -0,0 +1,14 @@
+package havecapital
+
+import "testing"
+
+func Test_CalculateHaveCapital(t *testing.T) {
+	feeCapital := 1000000.00
+	expectedFeePrice := 20000.00
+
+	actualFeePrice := CalculateHaveCapital(feeCapital)
+
+	if expectedFeePrice != actualFeePrice {
+		t.Errorf("Expected %v but i got %v", expectedFeePrice, actualFeePrice)
+	}
+}
