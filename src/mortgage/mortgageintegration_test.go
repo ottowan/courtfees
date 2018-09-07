41564489553a6cbb6307079577dbff798c768222
parinya <parinya.coj@gmail.com>
2018-09-07 16:24:23 +0700

    [Edited] สร้าง function คำนวนค่าบังคับจำนอง ทุนทรัพย์ระหว่าง 300k-50m

--- src/mortgage/mortgageintegration_test.go
+++ src/mortgage/mortgageintegration_test.go
@@ -11,3 +11,23 @@ func Test_CalculateMortgage_Input_feeCapital_500000_Sholud_Be_5000(t *testing.T)
 		t.Errorf("Expected %v but i got %v", expectedFeeCapital, actualFeeCapital)
 	}
 }
+
+func Test_CalculateMortgage_Input_feeCapital_20000000_Sholud_Be_100000(t *testing.T) {
+	feeCapital := 20000000.00
+	expectedFeeCapital := 100000.00
+	actualFeeCapital := CalculateMortgage(feeCapital)
+
+	if expectedFeeCapital != actualFeeCapital {
+		t.Errorf("Expected %v but i got %v", expectedFeeCapital, actualFeeCapital)
+	}
+}
+
+func Test_CalculateMortgage_Input_feeCapital_20000000_Sholud_Be_100000(t *testing.T) {
+	feeCapital := 20000000.00
+	expectedFeeCapital := 100000.00
+	actualFeeCapital := CalculateMortgage(feeCapital)
+
+	if expectedFeeCapital != actualFeeCapital {
+		t.Errorf("Expected %v but i got %v", expectedFeeCapital, actualFeeCapital)
+	}
+}
