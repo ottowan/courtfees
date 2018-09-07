c030f212ff1468c563f40df49e0e50038e369dd6
parinya <parinya.coj@gmail.com>
2018-09-07 15:35:54 +0700

    [Created] สร้าง function คำนวนค่าไม่มีทุนทรัพย์ตามมาตราอื่นๆ

--- src/noncapital/noncapitalintegration_test.go
+++ src/noncapital/noncapitalintegration_test.go
@@ -14,3 +14,16 @@ func Test_CalculateNonCapital_Input_feeType_noncapital_section_section288_1_feeC
 		t.Errorf("Expected %v but i got %v", expectedFeePrice, actualFeePrice)
 	}
 }
+
+func Test_CalculateNonCapital_Input_feeType_noncapital_section_blank_feeCapital_0_Should_Be_0(t *testing.T) {
+	section := ""
+	feeCapital := 200.00
+
+	expectedFeePrice := 200.00
+
+	actualFeePrice := CalculateNonCapital(section, feeCapital)
+
+	if expectedFeePrice != actualFeePrice {
+		t.Errorf("Expected %v but i got %v", expectedFeePrice, actualFeePrice)
+	}
+}
