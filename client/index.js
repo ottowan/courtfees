d89c41e16a07531f291f4f4e1e124fbf4fd1e187
parinya <parinya.coj@gmail.com>
2018-10-08 08:25:58 +0700

    [Edited] replace text by images

--- client/index.js
+++ client/index.js
@@ -1,47 +1,354 @@
 var express = require("express");
 var https = require("https");
-var http = require("http");
 var fs = require("fs");
-var path = require("path");
+var bodyParser = require("body-parser");
 var request = require("request");
-
-const fetch = require("node-fetch");
-
 
 const config = require("./config");
 
+//var endpoint = "https://localhost:8080";
 var endpoint = "https://localhost:8080";
 var app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/views"));
-
-// This line is from the Node.js HTTPS documentation.
-// var options = {
-//   key: fs.readFileSync("./cert/key.pem"),
-//   cert: fs.readFileSync("./cert/cert.pem"),
-//   passphrase: "password"
-// };
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 var options = {
-  pfx: fs.readFileSync('./cert/Cert.pfx'),
+  pfx: fs.readFileSync("./cert/Cert.pfx"),
   passphrase: config.certPassword()
 };
 
-
-// Create a service (the app object is just a callback).
+//Main : index
 app.get("/", (req, res) => {
-  res.render("index", {active : "index"});
+  res.render("index", { active: "index" });
 });
 
-// Create a service (the app object is just a callback).
+//Main : courtfees
 app.get("/courtfees", (req, res) => {
-  res.render("courtfees", {active : "courtfees"});
+  res.render("courtfees", { active: "courtfees" });
 });
-// Create a service (the app object is just a callback).
+
+
+//Main : arbitration
 app.get("/arbitration", (req, res) => {
-  res.render("arbitration", {active : "arbitration"});
+  res.render("arbitration", { active: "arbitration" });
+});
+
+//ProvincialCourt HaveCapital 
+var rootURL = "https://localhost:8080/"
+app.get("/courtfees/provincialcourt/havecapital/:feeCapital", (req, res) => {
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: rootURL+"provincialcourt/havecapital?feeCapital=" + req.params.feeCapital,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//ProvincialCourt NonCapital 
+app.get("/courtfees/provincialcourt/noncapital/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"provincialcourt/noncapital?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//ProvincialCourt mortgage 
+app.get("/courtfees/provincialcourt/mortgage/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"provincialcourt/mortgage?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//ProvincialCourt appeal or supreme 
+app.get("/courtfees/provincialcourt/appeal_or_supreme/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"provincialcourt/appealorsupreme?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+
+//ProvincialCourt non_and_havecapital 
+///courtfees/provincialcourt/non_and_havecapital/0
+app.get("/courtfees/provincialcourt/non_and_havecapital/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"provincialcourt/nonandhavecapital?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+
+
+
+
+//ProvincialCourt compensation 
+app.get("/courtfees/provincialcourt/compensation/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"provincialcourt/compensation?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+app.get("/courtfees/kwaengcourt/havecapital/:feeCapital", (req, res) => {
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: rootURL+"kwaengcourt/havecapital?feeCapital=" + req.params.feeCapital,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//kwaengcourt NonCapital 
+app.get("/courtfees/kwaengcourt/noncapital/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"kwaengcourt/noncapital?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//kwaengcourt mortgage 
+app.get("/courtfees/kwaengcourt/mortgage/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"kwaengcourt/mortgage?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+//kwaengcourt appeal or supreme 
+app.get("/courtfees/kwaengcourt/appeal_or_supreme/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"kwaengcourt/appealorsupreme?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+
+//kwaengcourt non_and_havecapital 
+///courtfees/kwaengcourt/non_and_havecapital/0
+app.get("/courtfees/kwaengcourt/non_and_havecapital/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"kwaengcourt/nonandhavecapital?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+
+
+
+
+//kwaengcourt compensation 
+app.get("/courtfees/kwaengcourt/compensation/:feeCapital", (req, res) => {
+
+  var URI = rootURL+"kwaengcourt/compensation?feeCapital=" + req.params.feeCapital
+  console.log(URI)
+  request.post(
+    {
+      headers: { "content-type": "application/json" },
+      url: URI,
+      body: JSON.stringify({
+        feeCapital: req.params.feeCapital
+      }),
+      rejectUnauthorized: false
+    },
+    (error, response, body) => {
+      if (error) {
+        res.json(JSON.parse(error));
+        return console.dir(error);
+      }
+      //console.dir(JSON.parse(body));
+      res.json(JSON.parse(body));
+    }
+  );
 });
 
 
